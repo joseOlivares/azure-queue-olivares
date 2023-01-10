a3b52f97fff46400c402c5b0140f8eb94dac7bac
José Luis Olivares <jolivares@gbm.net>
2023-01-10 15:40:24 -0600

    Agregando endpoint dequeue para queueIN

--- index.js
+++ index.js
@@ -12,50 +12,10 @@ app.use(express.json());
 app.use(require('./routes/index'));
 
 app.listen(PORT, (error) =>{
-    if(!error){
-        console.log("Server is Successfully Running,and App is listening on port "+ PORT);
-
-        //getQueueList();
-
-        /*let ciclo= setInterval(()=>{
-            getQueueLength();
-        },22000);*/
-        
-    } else{
-        console.log("Error occurred, server can't start", error);
-    }
-        
+        if(!error){
+            console.log("Server is Successfully Running, App is listening on port "+ PORT);    
+        } else{
+            console.log("Error occurred, server can't start", error);
+        }
     }
 );
-
-
-
-
-async function getQueueLength(){
-
-
-    // Get a QueueClient which will be used
-    // to create and manipulate a queue
-    const queueClient = queueServiceClient.getQueueClient(queueName); 
-
-    const properties = await queueClient.getProperties();
-
-    console.log("Longitud aproximada de la cola: ", properties.approximateMessagesCount);
-
-    if(properties.approximateMessagesCount>0){
-       
-       
-        /* Desencolando los primeros 5 mensajes */
-        // Get the first message in the queue
-       /* let receivedMessages = await queueClient.receiveMessages();
-        const firstMessage = receivedMessages.receivedMessageItems[0];
-
-        // Update the received message
-        await queueClient.updateMessage(firstMessage.messageId,firstMessage.popReceipt)
-        */
-    }
-
-
-
-
-}
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,21 @@ const { Router } = require('express');
 const router = Router();
 
 //Azure services 
-const queueServiceClient=require('../configuration/azure_queueServiceClient');
- 
-//Raiz
+const azService=require('../configuration/azure_queueService');
+const queueServiceClient=azService.queueServiceClient;
+const queueClientIN=azService.queueClientIN;
+
+/*
+* Raiz
+*/
 router.get('/', (req, res) => {
     res.status(200).json({"Ok": "Node Server - Azure queue working!"}
     );
 });
 
-//Lista de colas disponibles en el storageAccount
+/*
+* Obteber Lista de colas disponibles en el storageAccount
+*/
 router.get('/queue-list', async (req,res)=>{
     let queueList=[];
     console.log("Queues:");
@@ -19,21 +25,44 @@ router.get('/queue-list', async (req,res)=>{
             queueList.push(item.name);
             console.log(`- ${item.name}`);
         }  
-        res.status(200).json({"data":queueList});
+        res.status(200).json({"data":queueList,"status":1});
     } catch (error) {
-        res.status(400).json({"error":error});
-        console.log(error)
+        res.status(400).json({"error":error,"status":0});
+        console.log(error);
     }
 });
 
-
 /*
-async function getQueueList(){
-    console.log("Queues:");
-    for await (const item of queueServiceClient.listQueues()) {
-        console.log(`- ${item.name}`);
-    }
+* Quitar de la cola un mensaje
+*/
+router.get('/dequeue', async (req,res)=>{
 
-}*/
+    try {
+        const dequeueResponse = await queueClientIN.receiveMessages();
+
+        console.log('/dequeue');
+        console.log(`dequeueResponse.receivedMessageItems.length= ${dequeueResponse.receivedMessageItems.length}`);
+
+        if (dequeueResponse.receivedMessageItems.length == 1) {
+          const dequeueMessageItem = dequeueResponse.receivedMessageItems[0];
+          console.log(`Processing & deleting message with content: ${dequeueMessageItem.messageText}`);
+          const deleteMessageResponse = await queueClientIN.deleteMessage(
+            dequeueMessageItem.messageId,
+            dequeueMessageItem.popReceipt
+          );
+          console.log(
+            `Deleted message successfully, service assigned request ID: ${deleteMessageResponse.requestId}`
+          );
+          res.status(200).json({"data":deleteMessageResponse.requestId,"status":1});
+        }else{
+            console.log("QueueIN Empty");
+            res.status(200).json({"data":"QueueIN empty","status":1}); 
+        }     
+    } catch (error) {
+        res.status(400).json({"error":error,"status":0});
+        console.log(error);    
+    }
+});
+
  
 module.exports = router;
